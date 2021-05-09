7f948bde18302b456cafb79a50436b3f714717b9
Mauricio Sepulveda <mauricio.sepulveda@gmail.com>
2021-05-08 21:05:18 -0400

    bueno chat

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ async def event_ready():
     'Called once when the bot goes online.'
     print(f"{os.environ['BOT_NICK']} is online!")
     ws = bot._ws  # this is only needed to send messages within event_ready
-    await ws.send_privmsg(os.environ['CHANNEL'], f"/me has landed!, !bueno en el chat")
+    await ws.send_privmsg(os.environ['CHANNEL'], f"/bueno chat!")
 
 
 @bot.event
